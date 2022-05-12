21e12a29e2c343c3a119685744aa756b03c5f2bb
jpier1O <jeanpieres14@gmail.com>
2022-05-12 15:55:18 -0500

    fix in container carrousel

--- src/components/CarouselCardItem.js
+++ src/components/CarouselCardItem.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { View, Text, StyleSheet, Dimensions, Image } from "react-native";
 
-export const SLIDER_WIDTH = Dimensions.get("window").width + 80;
-export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
+export const SLIDER_WIDTH = Dimensions.get("window").width;
+export const ITEM_WIDTH = Math.round(SLIDER_WIDTH);
 
 const CarouselCardItem = ({ item, index }) => {
   return (
