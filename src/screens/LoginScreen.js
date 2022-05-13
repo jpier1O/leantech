a4aa6fdca8d20e4894ce8c7659014622b364d8c8
jpier1O <jeanpieres14@gmail.com>
2022-05-12 20:40:06 -0500

    remove unused things

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -60,7 +60,6 @@ export default function LoginScreen({ navigation }) {
         Login
       </Button>
       <View style={styles.row}>
-        <Text>Don’t have an account? </Text>
         <TouchableOpacity onPress={() => navigation.replace('RegisterScreen')}>
           <Text style={styles.link}>Sign up</Text>
         </TouchableOpacity>
